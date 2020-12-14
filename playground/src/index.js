800f6120b8754ac030e7ad3ef6f08747a54a2a09
Russell Johnston <rpjohnst@gmail.com>
2020-12-13 23:28:33 -0800

    Update JavaScript build tools

--- playground/src/index.js
+++ playground/src/index.js
@@ -4,11 +4,7 @@ import { clear } from "./print.js";
 import ace from "ace-builds";
 
 (async () => {
-  try {
-    await init(wasm);
-  } catch (error) {
-    console.error(error);
-  }
+  await init(wasm);
 })();
 
 ace.config.set("basePath", "module");
@@ -31,55 +27,3 @@ function exec(string) {
   clear();
   run(string);
 }
-
-/*
-show_debug_message("hello world")
-
-var list;
-list = ds_list_create()
-ds_list_add(list, 3, "foo")
-ds_list_add(list, 5)
-show_debug_message("list[| 0] =", ds_list_find_value(list, 0))
-show_debug_message("list[| 1] =", ds_list_find_value(list, 1))
-show_debug_message("list[| 2] =", ds_list_find_value(list, 2))
-ds_list_destroy(list)
-
-var map, key1, key2;
-map = ds_map_create()
-ds_map_add(map, 3, "foo")
-ds_map_add(map, "abc", "bar")
-key1 = ds_map_find_first(map)
-key2 = ds_map_find_next(map, key1)
-show_debug_message("map[?", key1, "] =", ds_map_find_value(map, key1))
-show_debug_message("map[?", key2, "] =", ds_map_find_value(map, key2))
-ds_map_destroy(map)
-
-var grid;
-grid = ds_grid_create(2, 2)
-ds_grid_set(grid, 0, 0, 1)
-ds_grid_set(grid, 1, 0, 2)
-ds_grid_set(grid, 0, 1, 3)
-ds_grid_set(grid, 1, 1, 4)
-show_debug_message("grid[# 0, 0] =", ds_grid_get(grid, 0, 0))
-show_debug_message("grid[# 1, 0] =", ds_grid_get(grid, 1, 0))
-show_debug_message("grid[# 0, 1] =", ds_grid_get(grid, 0, 1))
-show_debug_message("grid[# 1, 1] =", ds_grid_get(grid, 1, 1))
-ds_grid_destroy(grid)
-
-show_debug_message("object_index =", object_index)
-show_debug_message("id =", id)
-persistent = true
-show_debug_message("persistent =", persistent)
-
-for (i = 0; i < instance_count; i += 1) {
-    if instance_exists(instance_id[i]) {
-        show_debug_message(instance_id[i], "=>", instance_id[i].object_index)
-    }
-}
-
-show_debug_message("instance_find(1, 0) =>", instance_find(1, 0))
-show_debug_message("instance_exists(0) =>", instance_exists(0))
-show_debug_message("instance_exists(100002) =>", instance_exists(100002))
-show_debug_message("instance_exists(2) =>", instance_exists(2))
-show_debug_message("instance_number(1) =>", instance_number(1))
-*/
